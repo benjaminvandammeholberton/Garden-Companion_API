5824482802b93d40f22626ef76163d39bfd2bdc0
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-11-30 17:29:42 +0100

    Improve update function to handle date and area

--- app/services/vegetable_manager_service.py
+++ app/services/vegetable_manager_service.py
@@ -7,7 +7,6 @@ import json
 import pprint
 from typing import List
 from uuid import UUID
-from app.models.area_model import Area
 
 from app.models.vegetable_manager_model import VegetableManager
 from app.models.user_model import User
@@ -73,11 +72,28 @@ class VegetableManagerService:
         :param data: VegetableManager update data.
         :return: Updated vegetable.
         """
+        if data.area:
+            area_id = UUID(data.area)
+            area = await AreaService.retrieve_area(current_user=current_user, area_id=area_id)
+            if not area:
+                raise ValueError(f"Area with ID {data.area} not found.")
+            data.area = area
+
+        # List of date fields to process
+        date_fields = ['remove_date', 'sowing_date', 'planting_date', 'harvest_date']
+
+        # Convert each date field to datetime if it exists in the data
+        for field in date_fields:
+            date_value = getattr(data, field, None)
+            if date_value:
+                setattr(data, field, datetime.combine(date_value, datetime.min.time()))
+
         vegetable = await VegetableManagerService.retrieve_vegetable(current_user, vegetable_id)
         await vegetable.update({"$set": data.dict(exclude_unset=True)})
         vegetable.update_updated_at()
         await vegetable.save()
         return vegetable
+
 
     @staticmethod
     async def delete_vegetable(current_user: User, vegetable_id: UUID):
