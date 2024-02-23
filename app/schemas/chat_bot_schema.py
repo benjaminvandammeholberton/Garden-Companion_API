6ae79a2d5a83169e02a990a48453bd65f812984d
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-02-23 15:03:28 +0100

    Refactor code for Flake8 compliance and improved readability

---
+++ app/schemas/chat_bot_schema.py
@@ -0,0 +1,15 @@
+"""
+Chat-Bot related Pydantic models
+"""
+
+from pydantic import BaseModel, Field
+
+
+class ChatBotRequest(BaseModel):
+    """
+    """
+    user_input: str = Field(...,
+                            title='user chatbot request',
+                            max_length=100,
+                            alias='user-input'
+                            )
