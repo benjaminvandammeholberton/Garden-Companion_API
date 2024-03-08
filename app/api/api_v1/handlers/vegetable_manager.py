1dbfa41a528517d8be7e3b8b150e0d27e4d7e4ae
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-03-08 19:39:54 +0100

    add route to get vegetables from specific user

--- app/api/api_v1/handlers/vegetable_manager.py
+++ app/api/api_v1/handlers/vegetable_manager.py
@@ -1,16 +1,18 @@
 """
 
 """
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import List
 from uuid import UUID
 
 from app.core.dependencies import get_current_user
+from app.models.area_model import Area
 from app.schemas.vegetable_manager_schema import (
     VegetableManagerCreate,
     VegetableManagerOut,
     VegetableManagerUpdate
 )
+from app.services.user_service import UserService
 from app.services.vegetable_manager_service import VegetableManagerService
 from app.models.user_model import User
 from app.models.vegetable_manager_model import VegetableManager
@@ -32,6 +34,27 @@ async def list(current_user: User = Depends(get_current_user)):
     :return: List of vegetable_managers.
     """
     return await VegetableManagerService.list_vegetables(current_user)
+
+
+@vegetable_manager_router.get(
+    '/{username}',
+    summary='Get all vegetables from a specific user',
+    response_model=List[VegetableManagerOut]
+)
+async def get_vegetables_from_user(username: str, _=Depends(get_current_user)):
+    """
+    Endpoint to retrieve all vegetables_manager from a specific user.
+
+    :param username: the username of the user we want to get vegetables
+    :return: List of vegetable_manger
+    """
+    user = await UserService.get_user_by_username(username)
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Username doesn't exist"
+        )
+    return await VegetableManagerService.list_vegetables(user=user)
 
 
 @vegetable_manager_router.post(
@@ -79,8 +102,11 @@ async def retrieve(
     '/{vegetable_manager_id}',
     summary="Update vegetable_manager by vegetable_manager_id",
 )
-async def update(vegetable_manager_id: UUID, data: VegetableManagerUpdate,
-                 current_user: User = Depends(get_current_user)):
+async def update(
+    vegetable_manager_id: UUID,
+    data: VegetableManagerUpdate,
+    current_user: User = Depends(get_current_user)
+) -> VegetableManagerOut:
     """
     Endpoint to update a vegetable_manager by its ID.
 
